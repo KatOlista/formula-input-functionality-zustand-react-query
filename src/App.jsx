1566d9750e7652fd353e8a490988878d6bda2f45
KatOlista^^! <olya.katyushina@gmail.com>
2024-03-13 10:01:55 +0100

    create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Form, Header } from './components';
+
+export const App = () => {
+
+  return (
+    <>
+      <h1>Formula input functionality</h1>
+
+      <Header />
+
+      <Form />
+    </>
+  );
+};
